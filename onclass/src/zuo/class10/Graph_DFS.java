6166976d72d97496f9d555534a619298c6ae11ae
Wei <wb-wzy821124@alibaba-inc.com>
2023-10-16 10:51:50 +0800

    feat(图的深度遍历): DFS递归方法

--- onclass/src/zuo/class10/Graph_DFS.java
+++ onclass/src/zuo/class10/Graph_DFS.java
@@ -1,5 +1,7 @@
 package zuo.class10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Stack;
 
@@ -33,6 +35,45 @@ public class Graph_DFS {
                 }
             }
         }
+    }
+
+    public static void dfsRecursive(Node node, HashSet<Node> set){
+        if(node == null){
+            return;
+        }
+        set.add(node);
+        System.out.println(node.value);
+        for(Node next : node.nexts){
+            if(!set.contains(next)) {
+                dfsRecursive(next, set);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        Node node8 = new Node(8);
+
+        node1.nexts = new ArrayList<>(Arrays.asList(node2, node3, node4));
+        node2.nexts = new ArrayList<>(Arrays.asList(node5));
+        node5.nexts = new ArrayList<>(Arrays.asList(node6));
+        node3.nexts = new ArrayList<>(Arrays.asList(node7));
+        node4.nexts = new ArrayList<>(Arrays.asList(node8));
+        // 环
+        node8.nexts = new ArrayList<>(Arrays.asList(node1));
+
+        dfs(node1);
+
+        System.out.println("---------");
+        dfsRecursive(node1, new HashSet<>());
+
     }
 
 }
