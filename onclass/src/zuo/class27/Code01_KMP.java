babe9fdec709aa58d88c5708dacdf52fe0ad94bb
Wei <wb-wzy821124@alibaba-inc.com>
2024-06-11 19:01:01 +0800

    fix(kmp): 补充注释说明

--- onclass/src/zuo/class27/Code01_KMP.java
+++ onclass/src/zuo/class27/Code01_KMP.java
@@ -9,6 +9,23 @@ package zuo.class27;
 public class Code01_KMP {
 
 
+    /**
+     * 判断字符串s1是否包含s2，s1长度N >= s2长度M
+     * @param s1 ...abbstkscabbstks
+     *                            x位置
+     * @param s2    abbstkscabbstkz
+     *                            y位置
+     *              当s2的y位置z和s1的x位置s匹配不上，而z前面的串中最长前缀和最长后缀是abbstk，长度为6（通过next数组得到）
+     *              此时用s2中y位置的最长前缀abbstk的下一位s(y置为next[y] + 1)和s1的x位置s比较
+     *
+     *  复杂度:
+     *  由于 x < N 且 x-y < N
+     *  循环中三个分支:第一个x增，y增，x-y不变
+     *              第二个x增，y不变，x-y增
+     *              第三个x不变，y减小，x-y增
+     *  因此复杂度小于2N -> O(N)
+     * @return
+     */
     public static int getIndexOf(String s1, String s2){
         if(s1 == null || s2 == null || s2.length() < 1 || s1.length() < s2.length()){
             return -1;
@@ -30,10 +47,12 @@ public class Code01_KMP {
                 y = next[y];
             }
         }
+        // y越界了说明匹配成功 或者 x越界
         return y == str2.length ? x - y : -1;
     }
 
     // 求next[]数组 复杂度O(M) M为str2数组长度
+    // next[i]代表Str2第i个元素的最长前缀长度(也是最长后缀的长度)
     private static int[] getNextArray(char[] str2) {
         if(str2.length == 1){
             return new int[]{-1};
